84bb9e183b72cd4d4f0431e9643d8911fbfc64e3
Tristan Millington <tristan.millington@gmail.com>
2015-05-22 12:50:57 +0100

    Changed message model to have titles

--- messenger/models.py
+++ messenger/models.py
@@ -2,8 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Message(models.Model):
-    message_text = models.CharField(max_length=200)
+    message_text = models.TextField()
     time_sent = models.DateTimeField()
     read = models.BooleanField(default=False)
+    title = models.CharField(max_length=200)
     user_from = models.ForeignKey(User, related_name='user_from')
     user_to = models.ForeignKey(User, related_name='user_to')
--- messenger/views.py
+++ messenger/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Message
+
 
 # Create your views here.
 def index(request):
     return HttpResponse("Welcome to the messenger index")
+
+def messages(request):
+    user_messages = Message.objects.order_by('-time_sent')
+    context = {'user_messages' : user_messages}
+    return render(request, 'messages/messages.html', context)
