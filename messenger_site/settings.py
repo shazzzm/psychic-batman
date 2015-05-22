3d48dfb68a1a27d0b600a96ba1d50bca40c9b5b8
Tristan Millington <tristan.millington@gmail.com>
2015-05-22 11:51:46 +0100

    Added index page

--- messenger_site/settings.py
+++ messenger_site/settings.py
@@ -82,3 +82,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEMPLATE_DIRS = (
+    'messenger_site/templates',
+)
+
+LOGIN_REDIRECT_URL = '/'
--- messenger_site/urls.py
+++ messenger_site/urls.py
@@ -5,6 +5,8 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'messenger_site.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/$', 'django.contrib.auth.views.login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout'),
+    url(r'^messenger/', include('messenger.urls')),
 )
